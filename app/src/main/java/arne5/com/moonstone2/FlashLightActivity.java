c935a668e60a826de7728f790282e8ed29e30b0a
Dave Arneson <urvaius@gmail.com>
2017-06-21 08:56:30 -0700

    working need to change to myown pics and stuff

--- app/src/main/java/arne5/com/moonstone2/FlashLightActivity.java
+++ app/src/main/java/arne5/com/moonstone2/FlashLightActivity.java
@@ -1,13 +1,139 @@
 package arne5.com.moonstone2;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class FlashLightActivity extends AppCompatActivity {
+
+    private CameraManager mCameraManager;
+    private String mCameraId;
+    private ImageButton mTorchOnOffButton;
+    private Boolean isTorchOn;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("FlashLightActivity","OnCreate()");
         setContentView(R.layout.activity_flash_light);
+        mTorchOnOffButton = (ImageButton) findViewById(R.id.button_on_off);
+        isTorchOn = false;
+
+
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if(!isFlashAvailable) {
+            AlertDialog alert = new AlertDialog.Builder(FlashLightActivity.this).create();
+            alert.setTitle("Error !!");
+            alert.setMessage("your device does not support flash light");
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                    System.exit(0);
+                }
+            });
+            alert.show();
+            return;
+        }
+
+            mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            try {
+                mCameraId = mCameraManager.getCameraIdList()[0];
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+
+            mTorchOnOffButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        if (isTorchOn) {
+                            turnOffFlashLight();
+                            isTorchOn = false;
+                        } else {
+                            turnOnFlashLight();
+                            isTorchOn = true;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    public void turnOnFlashLight() {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, true);
+                playOnOffSound();
+                mTorchOnOffButton.setImageResource(R.drawable.on);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public void turnOffFlashLight() {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, false);
+                playOnOffSound();
+                mTorchOnOffButton.setImageResource(R.drawable.off);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void playOnOffSound(){
+
+        mp = MediaPlayer.create(FlashLightActivity.this, R.raw.flash_sound);
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // TODO Auto-generated method stub
+                mp.release();
+            }
+        });
+        mp.start();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(isTorchOn){
+            turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(isTorchOn){
+            turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(isTorchOn){
+            turnOnFlashLight();
+        }
+    }
+
 }
